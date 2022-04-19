7b84b11858b31c341e3e6da56e8af7d63d9c972f
AxelSalama <axelsalama2006@gmail.com>
2022-04-19 14:03:20 -0300

    Ejercicio Completado
    Ejercicio Completado

---
+++ Assets/Scripts/IngresoContra.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngresoContra : MonoBehaviour
+{
+    string contraCorrecta;
+    string contraUsuario;
+    public Text IngresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitomsj;
+    public void validarContra()
+    {
+        contraUsuario = IngresoUsuario.text;
+        if (contraUsuario == contraCorrecta)
+        {
+            cartelitomsj.SetActive(true);
+            textoMsj.text = "Bienvenido"; 
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitomsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraCorrecta = "12345";
+        cartelitomsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
